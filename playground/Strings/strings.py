78fb84ab7f541c06ebbf56c2b12ba8507667f326
MarcelloBB <marcellobelanda@gmail.com>
2021-12-09 14:14:07 -0300

    Finish [strings.py]

---
+++ playground/Strings/strings.py
@@ -0,0 +1,12 @@
+import fire
+
+class Str:
+    def __init__(self, char="-") -> None:
+        self.char = char
+
+    def print(self, size) -> str:
+            return self.char * size
+
+
+if __name__ == "__main__":
+    fire.Fire(Str)
